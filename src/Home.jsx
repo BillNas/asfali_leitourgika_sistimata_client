cb8e93e9d64a9beba756dc7ad6f1ef34dc7bffd1
BillNas <37874504+BillNas@users.noreply.github.com>
2024-02-02 15:20:52 +0200

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-export default function Component() {
+export default function Home() {
     return (
         <section className="w-full py-24 bg-gray-100 dark:bg-gray-800">
             <div className="container px-4 md:px-6">
